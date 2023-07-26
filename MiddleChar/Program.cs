92c2d05ae6f29967f69fd9f44ecdb2a852eb8635
M.Haris Khan <hkhan@cedrus.com>
2023-07-26 16:35:37 +0500

    Feat;MiddleChar

---
+++ MiddleChar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    public static string GetMiddle(string s)
+    {
+        int length = s.Length;
+        int middleIndex = length / 2;
+
+        if (length % 2 == 0)
+        {
+            return s.Substring(middleIndex - 1, 2);
+        }
+        else
+        {
+            return s[middleIndex].ToString();
+        }
+    }
+    public static void Main()
+    {
+        String Text;
+        Console.WriteLine("Enter a text");
+        Text = Convert.ToString(Console.ReadLine());
+        string value=GetMiddle(Text);
+        Console.WriteLine(value);
+        Console.ReadLine();
+    }
+}
